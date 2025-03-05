07720541fd6763a79d6ff30749bb8b9290cafeb9
Leonardo Guilmo Chin <leonardo.guilmochin@gmail.com>
2025-03-05 15:20:58 -0300

    feat: change page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Solar System",
+  title: "MetaIndústria Lab",
   description: "Realtime API demo",
   icons: {
     icon: "/icon.png",
